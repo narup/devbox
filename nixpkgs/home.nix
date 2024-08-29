f6bf1843dff753d00312161902c8cb2c58c4f047
Puran SS <178289+narup@users.noreply.github.com>
2024-08-28 20:09:14 -0700

    script fix

--- nixpkgs/home.nix
+++ nixpkgs/home.nix
@@ -1,11 +1,16 @@
 { config, pkgs, ... }:
+
 {
+
+  nixpkgs.config.allowUnfreePredicate = pkg: builtins.elem (lib.getName pkg) [
+    "vscode"
+  ];
+
   home.username = "puran";
   home.homeDirectory = "/home/puran";
   home.stateVersion = "22.11";
 
   home.packages = with pkgs; [
-    neovim
     alacritty
     kitty
     go
