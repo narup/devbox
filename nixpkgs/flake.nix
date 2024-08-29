8b27605ebb51828f024de3ca57f5855be88504f9
Puran SS <178289+narup@users.noreply.github.com>
2024-08-29 12:50:42 -0700

    more flake changes

--- nixpkgs/flake.nix
+++ nixpkgs/flake.nix
@@ -3,8 +3,14 @@
   description = "Flake definition for the entier devbox setup";
 
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
-    home-manager.url = "github:nix-community/home-manager";
+    # Pin our primary nixpkgs repository. This is the main nixpkgs repository
+    # we'll use for our configurations. Be very careful changing this because
+    # it'll impact your entire system.
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-24.05";
+    nixpkgs-unstable.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+    home-manager = {
+      url = "github:nix-community/home-manager/release-24.05";
+    };
   };
 
   
@@ -14,6 +20,15 @@
       dev_user = "puran";
       pkgs = nixpkgs.legacyPackages.${system};
     in {
+      nix = {
+        package = pkgs.nixVersions.latest;
+        extraOptions = ''
+          experimental-features = nix-command flakes
+          keep-outputs = true
+          keep-derivations = true
+        '';
+      };
+
       homeConfigurations.${dev_user} = home-manager.lib.homeManagerConfiguration {
         inherit pkgs;
         modules = [ ./home.nix ];
