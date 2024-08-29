b3e05cdc1a66ca1a8013a3424d13d7c428d4ebf7
Puran SS <178289+narup@users.noreply.github.com>
2024-08-28 20:15:54 -0700

    complete home.nix file

--- nixpkgs/home.nix
+++ nixpkgs/home.nix
@@ -4,16 +4,60 @@
 
   nixpkgs.config.allowUnfreePredicate = pkg: builtins.elem (lib.getName pkg) [
     "vscode"
+    "mongodb"
   ];
+
 
   home.username = "puran";
   home.homeDirectory = "/home/puran";
   home.stateVersion = "22.11";
 
   home.packages = with pkgs; [
-    alacritty
+    gcc
+    binutils
+    glibc
+    gnumake 
+    binutils 
+    coreutils
+    neovim
+    git
+    zsh
+	  antibody
+	  stow
+	  fzf
+    yazi
+	  ripgrep
+	  bat
+	  gnumake
+    btop
+    neofetch
+    bun
+	  direnv
     kitty
+    alacritty
+    zellij
+    wireguard-tools
     go
+    rbenv
+    ruby
+    elixir
+    asdf-vm
+    rustup
+    nasm
+    zig
+    postgresql_16
+    mongodb
+    vscode
+    brave
+    fx # terminal JSON viewer
+    ncdu
+    jq
+    yarn
+    htop
+    docker
+    flameshot # for screenshot
+    localsend # Airdrop alternative 
+    ollama
+    redis
   ];
-
 }
---
+++ nixpkgs/home_min.nix
@@ -0,0 +1,23 @@
+{ config, pkgs, ... }:
+
+{
+
+  nixpkgs.config.allowUnfreePredicate = pkg: builtins.elem (lib.getName pkg) [
+    "vscode"
+    "mongodb"
+  ];
+
+  home.username = "puran";
+  home.homeDirectory = "/home/puran";
+  home.stateVersion = "22.11";
+
+  home.packages = with pkgs; [
+    alacritty
+    kitty
+    neovim
+    zellij
+    zsh
+    go
+  ];
+
+}
