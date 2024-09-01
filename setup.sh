9ba35c55f7a2b2f40cda6a9216076d4b0259a064
Puran Singh <mepuran@icloud.com>
2024-09-01 11:47:52 -0700

    script change

--- setup.sh
+++ setup.sh
@@ -2,17 +2,12 @@
 
 echo "Starting the devbox setup"
 
-# Enable flakes
-mkdir -p ~/.config/nix
-
-echo "experimental-features = nix-command flakes" >>~/.config/nix/nix.conf
-
-echo "Installing Home Manager"
-
 # Setup Home Manager Nix config directory and copy the already packaged nix files 
 mkdir -p ~/.config/home-manager
 cp ./nix/flake.nix ~/.config/home-manager/flake.nix
 cp ./nix/home.nix ~/.config/home-manager/home.nix
+
+nix-shell -p home-manager 
 
 #install the packages defined in home.nix
 home-manager switch --flake ~/.config/home-manager
